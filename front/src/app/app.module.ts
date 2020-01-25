79def46e56b7b27aa2b691beda2603c6d3c36a61
Anirudh Mungre <anirudh.mungre16@gmail.com>
2020-01-25 17:45:58 -0500

    nav done

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './components/nav/nav.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
